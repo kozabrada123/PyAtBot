b8b1eae5e174a0a8aec0742080611d81111a0bc1
kozabrada123 <59031733+kozabrada123@users.noreply.github.com>
2022-04-22 15:15:10 +0200

    Un Accidentally doxxed myself lmao

--- Settings.py
+++ Settings.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 
 #General required settings
 
-Token = "ODc1NzM2MzE4NjE1ODc5NzQw.YRZ3AA.m53PfoKgJKoxWsa7w1m1b1m7GXM"
-Username = "AtBot"
-Password =  "koza1brada"
+Token = ****
+Username = ****
+Password =  ****
 
 #Discord settings
 prefix = "at."
